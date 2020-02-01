493656a2743eef2412030cac453fa6b245b7c20e
Samyocord <samyocord@gmail.com>
2020-02-01 13:45:02 +0100

    fixed fisch03's typos

--- bot/commands.js
+++ bot/commands.js
@@ -28,9 +28,9 @@ module.exports.commands = {
     onCall: function(msg, args) {
       var value = Math.floor(Math.random() * 101);
       var name = args[0];
-      if (name = "@Hanako" || name == "Hanako") {
+      if (name == "@Hanako" || name == "Hanako") {
 	msg.channel.send('${name} is 0% gay :gay_pride_flag:');
-      } else if (name = "Lars" || name == "Fisch03") {
+      } else if (name == "Lars" || name == "Fisch03") {
         msg.channel.send('${name} is 100% gay :gay_pride_flag:');
       }
       else {
